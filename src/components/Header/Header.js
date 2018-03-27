83355e3760a7568fd62ba2a281f442385692bbac
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-27 16:27:40 +0200

    Make application name clickable.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,16 @@ import Button from 'material-ui/Button';
 import Icon from 'material-ui/Icon';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import { Link } from 'react-router-dom';
 
 const Header = ({ isSignedIn }) => (
   <AppBar position="static" color="primary">
     <Toolbar>
       <Typography variant="title" color="inherit" style={{ flexGrow: 1 }}>
-        <Icon style={{ fontSize: '1.5em', verticalAlign: 'middle', marginRight: 10 }}>people</Icon>
-        Groupifier
+        <Link to="/">
+          <Icon style={{ fontSize: '1.5em', verticalAlign: 'middle', marginRight: 10 }}>people</Icon>
+          Groupifier
+        </Link>
       </Typography>
       {isSignedIn
         ? <Button color="inherit">Sign out</Button>
