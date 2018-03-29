d03a2ac2cc73372d481eaacc4ac9de22313e7543
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-29 20:09:29 +0200

    Make it clear if user has no upcoming competitions to manage.

--- src/components/CompetitionList/CompetitionList.js
+++ src/components/CompetitionList/CompetitionList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import List, { ListItem, ListItemText } from 'material-ui/List';
+import { LinearProgress } from 'material-ui/Progress';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import ListSubheader from 'material-ui/List/ListSubheader';
 import Paper from 'material-ui/Paper';
+import Icon from 'material-ui/Icon';
 
 import WcaApi from '../../logic/WcaApi';
 
@@ -10,26 +12,38 @@ export default class CompetitionList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      competitions: []
+      competitions: [],
+      loading: true
     }
   }
 
   componentDidMount() {
     WcaApi.getUpcomingManageableCompetitions()
-      .then(competitions => this.setState({ competitions }));
+      .then(competitions => this.setState({ competitions, loading: false }));
   }
 
   render() {
+    const { competitions, loading } = this.state;
+
     return (
       <div>
         <Paper>
           <List subheader={<ListSubheader>Your competitions</ListSubheader>}>
-            {this.state.competitions.map(competition =>
+            {!loading && competitions.length === 0 &&
+              <ListItem>
+                <ListItemIcon>
+                  <Icon>sentiment_very_dissatisfied</Icon>
+                </ListItemIcon>
+                <ListItemText primary="You have no upcoming competitions to manage." />
+              </ListItem>
+            }
+            {competitions.map(competition =>
               <ListItem key={competition.id} button component={Link} to={`/competitions/${competition.id}`}>
                 <ListItemText primary={competition.name} />
               </ListItem>
             )}
           </List>
+          {loading && <LinearProgress />}
         </Paper>
       </div>
     );
