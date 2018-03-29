4a11b5567dbd5b6cec43a9c94db13fe7d5de4fed
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-29 17:19:04 +0200

    Develop competition list further.

--- src/components/CompetitionList/CompetitionList.js
+++ src/components/CompetitionList/CompetitionList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import List, { ListItem, ListItemText } from 'material-ui/List';
+import ListSubheader from 'material-ui/List/ListSubheader';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 
@@ -21,9 +22,8 @@ export default class CompetitionList extends Component {
   render() {
     return (
       <div>
-        <Typography variant="title">Your competitions</Typography>
         <Paper>
-          <List>
+          <List subheader={<ListSubheader>Your competitions</ListSubheader>}>
             {this.state.competitions.map(competition =>
               <ListItem key={competition.id} button>
                 <ListItemText primary={competition.name} />
