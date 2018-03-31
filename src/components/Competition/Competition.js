3960f85039540723ab356528fd0a886151a12312
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-31 15:46:42 +0200

    Add visual loading to Competition component.

--- src/components/Competition/Competition.js
+++ src/components/Competition/Competition.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { LinearProgress } from 'material-ui/Progress';
+import Typography from 'material-ui/Typography';
 
 import WcaApi from '../../logic/WcaApi';
 
@@ -6,18 +8,26 @@ export default class Competition extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      wcif: null
+      wcif: null,
+      loading: true
     }
   }
 
   componentDidMount() {
     WcaApi.getCompetitionWcif(this.props.match.params.competitionId)
-      .then(wcif => this.setState({ wcif }))
+      .then(wcif => this.setState({ wcif, loading: false }))
   }
 
   render() {
+    const { wcif, loading } = this.state;
+
     return (
-      <div>{this.state.wcif && this.state.wcif.name}</div>
+      <div>
+        {loading && <LinearProgress />}
+        <Typography variant="display1">
+          {wcif && wcif.name}
+        </Typography>
+      </div>
     );
   }
 }
