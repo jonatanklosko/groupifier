2c1589e4451ec9345770a0990685e5d4f0a62ff8
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-05-03 22:12:57 +0200

    Turn PositiveIntegerInput into numeric input.

--- src/components/common/PositiveIntegerInput/PositiveIntegerInput.js
+++ src/components/common/PositiveIntegerInput/PositiveIntegerInput.js
@@ -2,6 +2,12 @@ import React, { PureComponent } from 'react';
 import TextField from 'material-ui/TextField';
 
 export default class PositiveIntegerInput extends PureComponent {
+  /* Prevent from entering characters like minus, plus and dot.
+     These don't trigger the change event for numeric input. */
+  handleKeyPress = event => {
+    if (event.key.match(/\D/)) event.preventDefault();
+  };
+
   handleTextFieldChange = event => {
     const newValue = event.target.value.length > 0 ? parseInt(event.target.value, 10) : null;
     if (newValue === null || (!Number.isNaN(newValue) && newValue >= 1)) {
@@ -13,7 +19,13 @@ export default class PositiveIntegerInput extends PureComponent {
     const { value, onChange, ...props } = this.props;
 
     return (
-      <TextField {...props} value={value === null ? '' : value} onChange={this.handleTextFieldChange} />
+      <TextField
+        {...props}
+        type="number"
+        value={value === null ? '' : value}
+        onKeyPress={this.handleKeyPress}
+        onChange={this.handleTextFieldChange}
+      />
     );
   }
 };
