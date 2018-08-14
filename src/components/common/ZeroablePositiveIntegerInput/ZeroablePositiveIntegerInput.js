683f23088807144645497db3edcbdf8af67340b5
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-08-14 03:24:07 +0200

    Use new React Ref API to focus the right input in ZeroablePositiveIntegerInput omponent.

--- src/components/common/ZeroablePositiveIntegerInput/ZeroablePositiveIntegerInput.js
+++ src/components/common/ZeroablePositiveIntegerInput/ZeroablePositiveIntegerInput.js
@@ -4,6 +4,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 import PositiveIntegerInput from '../PositiveIntegerInput/PositiveIntegerInput';
 
 export default class ZeroablePositiveIntegerInput extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
   handleCheckboxChange = event => {
     const { checked } = event.target;
     this.props.onChange(event, checked ? null : 0);
@@ -11,7 +16,7 @@ export default class ZeroablePositiveIntegerInput extends PureComponent {
 
   componentDidUpdate(prevProps) {
     if (prevProps.value === 0 && this.props.value !== 0) {
-      document.querySelector(`input[type="number"][name="${this.props.name}"]`).focus();
+      this.inputRef.current.focus();
     }
   }
 
@@ -20,7 +25,7 @@ export default class ZeroablePositiveIntegerInput extends PureComponent {
 
     return (
       <Fragment>
-        <PositiveIntegerInput {...props} value={value} disabled={value === 0 || disabled} />
+        <PositiveIntegerInput {...props} value={value} disabled={value === 0 || disabled} inputRef={this.inputRef} />
         <Checkbox name={props.name} checked={value !== 0} onChange={this.handleCheckboxChange} disabled={disabled} />
       </Fragment>
     );
