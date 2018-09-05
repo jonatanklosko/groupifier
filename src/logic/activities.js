fd81ef20f312416ab9ea810edbd1090fb7987cb8
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-09-05 20:55:22 +0200

    Simplify the populateActivitiesConfig function.

--- src/logic/activities.js
+++ src/logic/activities.js
@@ -24,6 +24,11 @@ export const isActivityConfigurable = activity => {
 const activityDuration = activity =>
   new Date(activity.endTime) - new Date(activity.startTime)
 
+const activityStations = (wcif, activity) => {
+  const room = wcif.schedule.venues[0].rooms.find(room => room.activities.includes(activity));
+  return getGroupifierData(room).stations;
+};
+
 const suggestedScramblerCount = stations =>
   Math.floor(Math.log2(stations + 1));
 
@@ -31,32 +36,31 @@ const suggestedRunnerCount = stations =>
   Math.floor(Math.log(stations + 2) / Math.log(3));
 
 export const populateActivitiesConfig = (wcif, expectedCompetitorsByRound, { assignScramblers, assignRunners, assignJudges }) => {
-  const activitiesWithStations = flatMap(wcif.schedule.venues[0].rooms, room =>
-    room.activities
-      .filter(isActivityConfigurable)
-      .map(activity => [activity, getGroupifierData(room).stations])
+  const activities = flatMap(wcif.schedule.venues[0].rooms, room =>
+    room.activities.filter(isActivityConfigurable)
   );
-  const activities = flatMap(wcif.events, wcifEvent => {
+  const activitiesWithConfig = flatMap(wcif.events, wcifEvent => {
     return flatMap(wcifEvent.rounds, round => {
       const competitors = expectedCompetitorsByRound[round.id];
-      const roundActivitiesWithStations = activitiesWithStations
-        .filter(([activity, stations]) => activity.activityCode.startsWith(round.id));
-      const densities = roundActivitiesWithStations
-        .map(([activity, stations]) => stations * activityDuration(activity));
+      const roundActivities = activities
+        .filter(activity => activity.activityCode.startsWith(round.id));
+      const densities = roundActivities
+        .map(activity => activityStations(wcif, activity) * activityDuration(activity));
       const densitiesSum = densities.reduce((x, y) => x + y, 0);
       const normalizedDensities = densities.map(density => densitiesSum !== 0 ? density / densitiesSum : 1 / densities.length);
-      return zip(roundActivitiesWithStations, normalizedDensities).map(([[activity, stations], density]) =>
-        setGroupifierData('Activity', activity, {
+      return zip(roundActivities, normalizedDensities).map(([activity, density]) => {
+        const stations = activityStations(wcif, activity);
+        return setGroupifierData('Activity', activity, {
           density,
           groups: suggestedGroupCount(Math.floor(density * competitors.length), wcifEvent.id, stations, 2),
           scramblers: assignScramblers ? suggestedScramblerCount(stations) : 0,
           runners: assignRunners ? suggestedRunnerCount(stations) : 0,
           assignJudges
         })
-      );
+      });
     });
   });
-  return activities.reduce(updateActivity, wcif);
+  return activitiesWithConfig.reduce(updateActivity, wcif);
 };
 
 export const updateActivity = (wcif, updatedActivity) =>
