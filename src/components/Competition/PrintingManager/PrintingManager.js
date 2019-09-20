7ecf3d2212c39bda1cee375d3c228db1203b6ef5
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-09-21 01:19:52 +0200

    Add a notification to the printing screen if some rounds are missing task assignment.

--- src/components/Competition/PrintingManager/PrintingManager.js
+++ src/components/Competition/PrintingManager/PrintingManager.js
@@ -3,17 +3,49 @@ import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
+import pink from '@material-ui/core/colors/pink';
 
 import Scorecards from './Scorecards/Scorecards';
 import CompetitorCards from './CompetitorCards/CompetitorCards';
+import {
+  roundsMissingAssignments,
+  activityCodeToName,
+} from '../../../logic/activities';
 
 const PrintingManager = ({ wcif }) => {
   const [tabValue, setTabValue] = useState(0);
 
+  const roundsMissingAssignmentsNames = roundsMissingAssignments(wcif).map(
+    round => activityCodeToName(round.id)
+  );
+
   return (
     <Grid container spacing={1} justify="flex-end">
+      <Grid item xs={12}>
+        {roundsMissingAssignmentsNames.length > 0 && (
+          <SnackbarContent
+            style={{ maxWidth: 'none' }}
+            message={`
+              The following rounds are missing task assignments:
+              ${roundsMissingAssignmentsNames.join(', ')}.
+              You may want to assign them first.
+            `}
+            action={
+              <Button
+                component={Link}
+                to={`/competitions/${wcif.id}/groups`}
+                style={{ color: pink[500] }}
+                size="small"
+              >
+                Manage groups
+              </Button>
+            }
+          />
+        )}
+      </Grid>
       <Grid item xs={12}>
         <AppBar position="static" color="default">
           <Tabs
