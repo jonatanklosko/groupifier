37089c391dc4f2525a5112ef462d34e0b4ac37f3
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-09-21 23:25:12 +0200

    Add note about WCA Live.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
+import Link from '@material-ui/core/Link';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
 
 const Header = ({ isSignedIn, onSignIn, onSignOut }) => (
   <AppBar position="static" color="primary">
     <Toolbar>
-      <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
-        <Link to={isSignedIn ? '/competitions' : '/'}>
+      <Typography variant="h6" color="inherit">
+        <RouterLink to={isSignedIn ? '/competitions' : '/'}>
           <Icon style={{ fontSize: '1.5em', verticalAlign: 'middle', marginRight: 10 }}>people</Icon>
           Groupifier
+        </RouterLink>
+      </Typography>
+      <Typography style={{ flexGrow: 1, marginLeft: 16 }} variant="body2" color="inherit">
+        {`This version is meant to be used with Cubecomps. Navigate `}
+        <Link
+          href="https://groupifier.jonatanklosko.com"
+          color="inherit"
+          style={{ fontWeight: 500 }}
+        >
+          {`here `}
+          <span role="img" aria-label="rocket">🚀</span>
         </Link>
+        {` if you're using WCA Live.`}
       </Typography>
       {isSignedIn
         ? <Button color="inherit" onClick={onSignOut}>Sign out</Button>
