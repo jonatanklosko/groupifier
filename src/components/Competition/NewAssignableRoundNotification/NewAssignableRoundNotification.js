02714013622db6e60e5cdf95295b075f2dcf8179
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-01-31 21:53:25 +0100

    Update dependencies.

--- src/components/Competition/NewAssignableRoundNotification/NewAssignableRoundNotification.js
+++ src/components/Competition/NewAssignableRoundNotification/NewAssignableRoundNotification.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import SnackbarContent from '@material-ui/core/SnackbarContent';
 import pink from '@material-ui/core/colors/pink';
