098f952e1f0847b2143f4afdfdfb62ce45c17dbe
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-10-17 01:41:03 +0800

    Add a workaround for thumbnail error on WCIF post

--- src/logic/wca-api.js
+++ src/logic/wca-api.js
@@ -26,7 +26,16 @@ export const saveWcifChanges = (previousWcif, newWcif) => {
     key => previousWcif[key] !== newWcif[key]
   );
   if (keysDiff.length === 0) return Promise.resolve();
-  return updateWcif(newWcif.id, pick(newWcif, keysDiff));
+
+  let wcifDiff = pick(newWcif, keysDiff);
+
+  // TODO: remove once WCIF thumbnail issue is resolved
+  if (wcifDiff.persons) {
+    const persons = wcifDiff.persons.map(({ avatar, ...person }) => person);
+    wcifDiff = { ...wcifDiff, persons };
+  }
+
+  return updateWcif(newWcif.id, wcifDiff);
 };
 
 const wcaApiFetch = (path, fetchOptions = {}) => {
