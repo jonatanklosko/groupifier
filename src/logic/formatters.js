64837989217677b365833c7b41f523cc6110e488
Jonatan Kłosko <jonatanklosko@gmail.com>
2026-04-27 13:49:17 +0200

    Add workaround for inconsistent cutoff value field

--- src/logic/formatters.js
+++ src/logic/formatters.js
@@ -2,12 +2,16 @@ import { parseActivityCode } from './activities';
 import { shortEventNameById } from './events';
 
 export const cutoffToString = (cutoff, eventId) => {
+  // TODO: currently WCIF v2 still returns attemptResult, rather than resultValue.
+  // This can be simplified once it is always resultValue.
+  const value = cutoff.resultValue || cutoff.attemptResult;
+
   if (eventId === '333mbf') {
-    return `> ${multibldResultValueToPoints(cutoff.resultValue)} points`;
+    return `> ${multibldResultValueToPoints(value)} points`;
   } else if (eventId === '333fm') {
-    return `< ${cutoff.resultValue} moves`;
+    return `< ${value} moves`;
   } else {
-    return `< ${centisecondsToClockFormat(cutoff.resultValue)}`;
+    return `< ${centisecondsToClockFormat(value)}`;
   }
 };
 
